3a5d9c906ff1b7f10a25e58a5ca29d08fc00b7d8
Adam Erispaha <Adam.Erispaha@water.gov>
2016-06-20 14:20:52 -0400

    working code for generating report images with new dedicated modules

--- swmmio/utils/functions.py
+++ swmmio/utils/functions.py
@@ -1,5 +1,5 @@
 from swmmio.defs.inpheaders import inp_header_dict, rpt_header_dict
-
+from collections import deque
 
 def random_alphanumeric(n=6):
 	import random
@@ -54,14 +54,25 @@ def complete_rpt_headers (rptfilepath, rpt_header_dict=rpt_header_dict):
     """
     foundheaders= {}
     order = []
-    #print inp_header_dict
     with open(rptfilepath) as f:
+        buff3line = deque()
         for line in f:
-            if '[' and ']' in line:
-                h = line.strip()
+
+
+            #maintains a 3 line buffer and looks for instances where
+            #a top and bottom line have '*****' and records the middle line
+            #typical of section headers in RPT files
+            buff3line.append(line)
+            if len(buff3line) > 3:
+                buff3line.popleft()
+
+            if ('***********'in buff3line[0] and
+				'***********'in buff3line[2] and
+				len(buff3line[1].strip()) > 0):
+                h = buff3line[1].strip()
                 order.append(h)
-                if h in inp_header_dict:
-                    foundheaders.update({h:inp_header_dict[h]})
+                if h in rpt_header_dict:
+                    foundheaders.update({h:rpt_header_dict[h]})
                 else:
                     foundheaders.update({h:'blob'})
 
--- swmmio/utils/text.py
+++ swmmio/utils/text.py
@@ -2,13 +2,20 @@
 #STANDARD READING AND WRITING OF TEXT FILES (E.G. .INP AND .RPT)
 
 import os
-from swmmio.utils.functions import random_alphanumeric, complete_inp_headers
+from swmmio.utils.functions import random_alphanumeric, complete_inp_headers, complete_rpt_headers
 
 
 #default file path suffix
 txt = '.txt'
 
-def extract_section_from_file(filepath, sectionheader, cleanheaders=False, startfile=False):
+def extract_section_from_file(filepath, sectionheader, cleanheaders=True, startfile=False, headerdefs=None):
+    f_extension = os.path.splitext(filepath)[1]
+    if f_extension == '.inp':
+        return extract_section_from_inp(filepath, sectionheader, cleanheaders, startfile, headerdefs)
+    if f_extension == '.rpt':
+        return extract_section_from_rpt(filepath, sectionheader, cleanheaders, startfile, headerdefs)
+
+def extract_section_from_inp(filepath, sectionheader, cleanheaders=True, startfile=False, headerdefs=None):
     """
     INPUT path to text file (inp, rpt, etc) and a text string
     matchig the section header of the to be extracted
@@ -21,8 +28,11 @@ def extract_section_from_file(filepath, sectionheader, cleanheaders=False, start
     """
 
     #headers = she.complete_inp_headers(inp)['headers']
+    if not headerdefs:
+        allheaders = complete_inp_headers(filepath)['headers']
+    else:
+        allheaders = headerdefs['headers']
 
-    allheaders = complete_inp_headers(filepath)['headers']
     with open(filepath) as f:
         startfound = False
         endfound = False
@@ -46,14 +56,16 @@ def extract_section_from_file(filepath, sectionheader, cleanheaders=False, start
 
                 if startfound:
                     newf.write(line)
-
+    if not startfound:
+        #input section header was not found in the file
+        return None
     if startfile:
         os.startfile(outfilepath)
 
     return outfilepath
 
 
-def extract_section_from_rpt(filepath, sectionheader, cleanheaders=False, startfile=False):
+def extract_section_from_rpt(filepath, sectionheader, cleanheaders=False, startfile=False, headerdefs=None):
     """
     INPUT path to rpt file and a text string
     matchig the section header of the to be extracted
@@ -64,4 +76,56 @@ def extract_section_from_rpt(filepath, sectionheader, cleanheaders=False, startf
     optionally inserts row at begining of file with one-line headers that are
     defined in swmmio.defs.inpheaders (useful for creating dataframes)
     """
-    pass
+    if not headerdefs:
+        allheaders = complete_rpt_headers(filepath)['headers']
+    else:
+        allheaders = headerdefs['headers']
+
+    with open(filepath) as f:
+
+
+        wd = os.path.dirname(filepath)
+        newfname = sectionheader + '_' + random_alphanumeric(6)
+        outfilepath = os.path.join(wd, newfname + txt)
+
+        startfound = False #where we see the section header name
+        datastartfound = False #where we see actual data after the header text
+        endfound = False
+        hyphencount = 0 #count how many hyphen rows we pass after startfound
+
+        with open(outfilepath, 'w') as newf:
+
+            for line in f:
+
+                if startfound and line.strip() in allheaders:
+                    endfound = True
+                    break
+                elif not startfound and sectionheader in line:
+                    startfound = True
+                    #replace line with usable headers
+
+                    if cleanheaders and [sectionheader] != 'blob':
+                        line = allheaders[sectionheader] + '\n'
+                        newf.write(line)
+
+                if startfound:
+                    if hyphencount == 2:
+                        datastartfound = True
+                        newf.write(line)
+                        if len(line.strip()) == 0:
+                            #if line is blank after we've found the data,
+                            #we can be confident that we've found the end
+                            break
+                    if '------------' in line:
+                        #track how many rows with hyphens are passed
+                        #two rows should occur before actual data is written
+                        hyphencount += 1
+
+    if not startfound:
+        #input section header was not found in the file
+        return None
+    if startfile:
+        os.startfile(outfilepath)
+
+    return outfilepath
+    #pass
