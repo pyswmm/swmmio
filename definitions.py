979bd33942506e0eea030c624b8986a4a46e63e9
Adam Erispaha <aerispaha@gmail.com>
2017-03-12 18:22:02 -0400

    added linux binary

--- definitions.py
+++ definitions.py
@@ -4,7 +4,10 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 #path to the SWMM5 executable used within the run_models module
-SWMM_ENGINE_PATH = os.path.join(ROOT_DIR, 'swmm5_engines', 'swmm5_22.exe')
+if os.name = 'posix':
+    SWMM_ENGINE_PATH = os.path.join(ROOT_DIR, 'lib', 'linux', 'swmm5')
+else:
+    SWMM_ENGINE_PATH = os.path.join(ROOT_DIR, 'lib', 'windows', 'swmm5_22.exe')
 
 #path to the default geodatabase. used for some arcpy functions and parcel calcs
 GEODATABASE = r'C:\Data\ArcGIS\GDBs\LocalData.gdb'
