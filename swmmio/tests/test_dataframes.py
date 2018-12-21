b0abce148d54c0c9f115aa9b8990b7c00c2df631
Adam Erispaha <aerispaha@gmail.com>
2018-12-20 19:09:27 -0500

    updated test to reflect test model

--- swmmio/tests/test_dataframes.py
+++ swmmio/tests/test_dataframes.py
@@ -52,4 +52,4 @@ def test_model_to_networkx():
 
     assert(G['J2']['J3']['C2.1']['Length'] == 666)
     assert(G['J1']['J2']['C1:C2']['Length'] == 244.63)
-    assert(round(G.node['J2']['InvertElev'], 3) == 13.392)
+    assert(round(G.node['J2']['InvertElev'], 3) == 13.0)
