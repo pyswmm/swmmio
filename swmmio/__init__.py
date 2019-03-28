5b3c8fdf922a4250f6587ff8168fdc2b0bb166ba
Adam Erispaha <aerispaha@gmail.com>
2019-03-28 10:34:26 -0400

    set dev version num

--- swmmio/__init__.py
+++ swmmio/__init__.py
@@ -5,7 +5,7 @@ import swmmio.core as swmmio
 '''Python SWMM Input/Output Tools'''
 
 
-VERSION_INFO = (0, 3, 4, 'post1') # post release to include json in manifest
+VERSION_INFO = (0, 3, 5, 'dev') 
 __version__ = '.'.join(map(str, VERSION_INFO))
 __author__ = 'Adam Erispaha'
 __copyright__ = 'Copyright (c) 2016'
