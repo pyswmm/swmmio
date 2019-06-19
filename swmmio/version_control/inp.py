4150018286b1703d73c6300cd9898187cbff94a5
Adam Erispaha <aerispaha@gmail.com>
2019-06-18 21:21:10 -0400

    pep8 formatting

--- swmmio/version_control/inp.py
+++ swmmio/version_control/inp.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import os
 import sys
 from copy import deepcopy
+
 if sys.version_info[0] < 3:
     from io import StringIO
 else:
@@ -27,20 +28,20 @@ class BuildInstructions(object):
 
     def __init__(self, build_instr_file=None):
 
-        #create a change object for each section that is different from baseline
+        # create a change object for each section that is different from baseline
         self.instructions = {}
         self.metadata = {}
         if build_instr_file:
-            #read the instructions and create a dictionary of Change objects
+            # read the instructions and create a dictionary of Change objects
             allheaders = funcs.complete_inp_headers(build_instr_file)
             instructions = {}
             for section in allheaders['order']:
                 change = INPDiff(build_instr_file=build_instr_file, section=section)
-                instructions.update({section:change})
+                instructions.update({section: change})
 
             self.instructions = instructions
 
-            #read the meta data
+            # read the meta data
             self.metadata = vc_utils.read_meta_data(build_instr_file)
 
     def __add__(self, other):
@@ -50,16 +51,15 @@ class BuildInstructions(object):
                 new_change = change_obj + other.instructions[section]
                 bi.instructions[section] = new_change
             else:
-                #section doesn't exist in other, maintain current instructions
+                # section doesn't exist in other, maintain current instructions
                 bi.instructions[section] = change_obj
 
         for section, change_obj in other.instructions.items():
             if section not in self.instructions:
                 bi.instructions[section] = change_obj
 
-
-        #combine the metadata
-        #deepcopy so child structures aren't linked to original
+        # combine the metadata
+        # deepcopy so child structures aren't linked to original
         bi.metadata = deepcopy(self.metadata)
         otherbaseline = other.metadata['Parent Models']['Baseline']
         otheralternatives = other.metadata['Parent Models']['Alternatives']
@@ -70,7 +70,7 @@ class BuildInstructions(object):
         return bi
 
     def __radd__(self, other):
-        #this is so we can call sum() on a list of build_instructions
+        # this is so we can call sum() on a list of build_instructions
         if other == 0:
             return self
         else:
@@ -83,7 +83,7 @@ class BuildInstructions(object):
         if not os.path.exists(dir):
             os.makedirs(dir)
         filepath = os.path.join(dir, filename)
-        with open (filepath, 'w') as f:
+        with open(filepath, 'w') as f:
             vc_utils.write_meta_data(f, self.metadata)
             for section, change_obj in self.instructions.items():
                 section_df = pd.concat([change_obj.removed, change_obj.altered, change_obj.added])
@@ -97,34 +97,35 @@ class BuildInstructions(object):
         """
         basemodel = swmmio.Model(baseline_dir)
         allheaders = funcs.complete_inp_headers(basemodel.inp.path)
-        #new_inp = os.path.join(target_dir, 'model.inp')
-        with open (target_path, 'w') as f:
+        # new_inp = os.path.join(target_dir, 'model.inp')
+        with open(target_path, 'w') as f:
             for section in allheaders['order']:
 
-                #check if the section is not in problem_sections and there are changes
-                #in self.instructions and commit changes to it from baseline accordingly
+                # check if the section is not in problem_sections and there are changes
+                # in self.instructions and commit changes to it from baseline accordingly
                 if (section not in problem_sections
-                    and allheaders['headers'][section] != 'blob'
-                    and section in self.instructions):
+                        and allheaders['headers'][section] != 'blob'
+                        and section in self.instructions):
 
-                    #df of baseline model section
+                    # df of baseline model section
                     basedf = create_dataframeINP(basemodel.inp.path, section)
 
-                    #grab the changes to
+                    # grab the changes to
                     changes = self.instructions[section]
 
-                    #remove elements that have alterations and or tagged for removal
+                    # remove elements that have alterations and or tagged for removal
                     remove_ids = changes.removed.index | changes.altered.index
                     new_section = basedf.drop(remove_ids)
 
-                    #add elements
+                    # add elements
                     new_section = pd.concat([new_section, changes.altered, changes.added])
                 else:
-                    #section is not well understood or is problematic, just blindly copy
+                    # section is not well understood or is problematic, just blindly copy
                     new_section = create_dataframeINP(basemodel.inp.path, section=section)
 
-                #write the section
+                # write the section
                 vc_utils.write_inp_section(f, allheaders, section, new_section)
+
 
 class INPDiff(object):
     """
@@ -136,40 +137,41 @@ class INPDiff(object):
 
 
     """
+
     def __init__(self, model1=None, model2=None, section='[JUNCTIONS]', build_instr_file=None):
 
         if model1 and model2:
             df1 = create_dataframeINP(model1.inp.path, section)
             df2 = create_dataframeINP(model2.inp.path, section)
 
-            #BUG -> this fails if a df1 or df2 is None i.e. if a section doesn't exist in one model
+            # BUG -> this fails if a df1 or df2 is None i.e. if a section doesn't exist in one model
             added_ids = df2.index.difference(df1.index)
             removed_ids = df1.index.difference(df2.index)
 
-            #find where elements were changed (but kept with same ID)
-            common_ids = df1.index.difference(removed_ids) #original - removed = in common
-            #both dfs concatenated, with matched indices for each element
+            # find where elements were changed (but kept with same ID)
+            common_ids = df1.index.difference(removed_ids)  # original - removed = in common
+            # both dfs concatenated, with matched indices for each element
             full_set = pd.concat([df1.loc[common_ids], df2.loc[common_ids]])
             # remove whitespace
             full_set = full_set.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-            #drop dupes on the set, all things that did not changed should have 1 row
+            # drop dupes on the set, all things that did not changed should have 1 row
             changes_with_dupes = full_set.drop_duplicates()
-            #duplicate indicies are rows that have changes, isolate these
+            # duplicate indicies are rows that have changes, isolate these
             # idx[idx.duplicated()].unique()
-            changed_ids = changes_with_dupes.index[changes_with_dupes.index.duplicated()].unique() #.get_duplicates()
+            changed_ids = changes_with_dupes.index[changes_with_dupes.index.duplicated()].unique()  # .get_duplicates()
 
             added = df2.loc[added_ids].copy()
-            added['Comment'] = 'Added'# from model {}'.format(model2.inp.path)
+            added['Comment'] = 'Added'  # from model {}'.format(model2.inp.path)
             added['Origin'] = model2.inp.path
 
             altered = df2.loc[changed_ids].copy()
-            altered['Comment'] = 'Altered'# in model {}'.format(model2.inp.path)
+            altered['Comment'] = 'Altered'  # in model {}'.format(model2.inp.path)
             altered['Origin'] = model2.inp.path
 
             removed = df1.loc[removed_ids].copy()
-            #comment out the removed elements
-            #removed.index = ["; " + str(x) for x in removed.index]
-            removed['Comment'] = 'Removed'# in model {}'.format(model2.inp.path)
+            # comment out the removed elements
+            # removed.index = ["; " + str(x) for x in removed.index]
+            removed['Comment'] = 'Removed'  # in model {}'.format(model2.inp.path)
             removed['Origin'] = model2.inp.path
 
             self.old = df1
@@ -179,8 +181,8 @@ class INPDiff(object):
             self.altered = altered
 
         if build_instr_file:
-            #if generating from a build instructions file, do this (more efficient)
-            df = create_dataframeBI(build_instr_file, section = section)
+            # if generating from a build instructions file, do this (more efficient)
+            df = create_dataframeBI(build_instr_file, section=section)
 
             self.added = df.loc[df['Comment'] == 'Added']
             self.removed = df.loc[df['Comment'] == 'Removed']
@@ -196,6 +198,7 @@ class INPDiff(object):
 
         return change
 
+
 def generate_inp_from_diffs(basemodel, inpdiffs, target_dir):
     """
     create a new inp with respect to a baseline inp and changes instructed
@@ -206,11 +209,11 @@ def generate_inp_from_diffs(basemodel, inpdiffs, target_dir):
     NOTE THIS ISN'T USED ANYWHERE. DELETE ????
     """
 
-    #step 1 --> combine the diff/build instructions
+    # step 1 --> combine the diff/build instructions
     allheaders = funcs.complete_inp_headers(basemodel.inp.path)
     combi_build_instr_file = os.path.join(target_dir, 'build_instructions.txt')
     newinp = os.path.join(target_dir, 'new.inp')
-    with open (combi_build_instr_file, 'w') as f:
+    with open(combi_build_instr_file, 'w') as f:
         for header in allheaders['order']:
             s = ''
             section_header_written = False
@@ -230,28 +233,28 @@ def generate_inp_from_diffs(basemodel, inpdiffs, target_dir):
                                                            skipheaders=True)
 
                 if sect_s:
-                    #remove the extra space between data in the same table
-                    #coming from diffrent models.
-                    if sect_s[-2:] == '\n\n':   #NOTE Check this section...
+                    # remove the extra space between data in the same table
+                    # coming from diffrent models.
+                    if sect_s[-2:] == '\n\n':  # NOTE Check this section...
                         s += sect_s[:-1]
                     else:
                         s += sect_s
 
             f.write(s + '\n')
 
-    #step 2 --> clean up the new combined diff instructions
+    # step 2 --> clean up the new combined diff instructions
     # df_dict = clean_inp_diff_formatting(combi_build_instr_file) #makes more human readable
 
-    #step 3 --> create a new inp based on the baseline, with the inp_diff
-    #instructions applied
-    with open (newinp, 'w') as f:
+    # step 3 --> create a new inp based on the baseline, with the inp_diff
+    # instructions applied
+    with open(newinp, 'w') as f:
         for section in allheaders['order']:
             print(section)
             if section not in problem_sections and allheaders['headers'][section] != 'blob':
-                #check if a changes from baseline spreadheet exists, and use this
-                #information if available to create the changes array
+                # check if a changes from baseline spreadheet exists, and use this
+                # information if available to create the changes array
                 df = create_dataframeINP(basemodel.inp.path, section)
-                df['Origin'] = '' #add the origin column if not there
+                df['Origin'] = ''  # add the origin column if not there
                 if section in df_dict:
                     df_change = df_dict[section]
                     ids_to_drop = df_change.loc[df_change['Comment'].isin(['Removed', 'Altered'])].index
@@ -259,16 +262,14 @@ def generate_inp_from_diffs(basemodel, inpdiffs, target_dir):
                     df = df.append(df_change.loc[df_change['Comment'].isin(['Added', 'Altered'])])
                 new_section = df
             else:
-                #blindly copy this section from the base model
+                # blindly copy this section from the base model
                 new_section = create_dataframeINP(basemodel.inp.path, section=section)
 
-            #write the section into the inp file and the excel file
+            # write the section into the inp file and the excel file
             vc_utils.write_inp_section(f, allheaders, section, new_section)
 
 
-
 def create_inp_build_instructions(inpA, inpB, path, filename, comments=''):
-
     """
     pass in two inp file paths and produce a spreadsheet showing the differences
     found in each of the INP sections. These differences should then be used
@@ -283,7 +284,7 @@ def create_inp_build_instructions(inpA, inpB, path, filename, comments=''):
     modela = swmmio.Model(inpA)
     modelb = swmmio.Model(inpB)
 
-    #create build insructions folder
+    # create build insructions folder
     if not os.path.exists(path):
         os.makedirs(path)
     filepath = os.path.join(path, filename) + '.txt'
@@ -292,27 +293,27 @@ def create_inp_build_instructions(inpA, inpB, path, filename, comments=''):
     # vc_utils.create_change_info_sheet(excelwriter, modela, modelb)
 
     problem_sections = ['[TITLE]', '[CURVES]', '[TIMESERIES]', '[RDII]', '[HYDROGRAPHS]']
-    with open (filepath, 'w') as newf:
+    with open(filepath, 'w') as newf:
 
-        #write meta data
+        # write meta data
         metadata = {
-            #'Baseline Model':modela.inp.path,
-            #'ID':filename,
-            'Parent Models':{
-                            'Baseline':{inpA:vc_utils.modification_date(inpA)},
-                            'Alternatives':{inpB:vc_utils.modification_date(inpB)}
-                            },
-            'Log':{filename:comments}
-            }
-        #print metadata
+            # 'Baseline Model':modela.inp.path,
+            # 'ID':filename,
+            'Parent Models': {
+                'Baseline': {inpA: vc_utils.modification_date(inpA)},
+                'Alternatives': {inpB: vc_utils.modification_date(inpB)}
+            },
+            'Log': {filename: comments}
+        }
+        # print metadata
         vc_utils.write_meta_data(newf, metadata)
         for section in allsections_a['order']:
             if section not in problem_sections:
-                #calculate the changes in the current section
+                # calculate the changes in the current section
                 changes = INPDiff(modela, modelb, section)
                 data = pd.concat([changes.removed, changes.added, changes.altered])
-                #vc_utils.write_excel_inp_section(excelwriter, allsections_a, section, data)
-                vc_utils.write_inp_section(newf, allsections_a, section, data, pad_top=False, na_fill='NaN') #na fill fixes SNOWPACK blanks spaces issue
-
+                # vc_utils.write_excel_inp_section(excelwriter, allsections_a, section, data)
+                vc_utils.write_inp_section(newf, allsections_a, section, data, pad_top=False,
+                                           na_fill='NaN')  # na fill fixes SNOWPACK blanks spaces issue
 
     # excelwriter.save()
