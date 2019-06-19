02df9c9059b8bba03dedeb72e497ec7d70386c73
Adam Erispaha <aerispaha@gmail.com>
2019-06-19 12:13:50 -0400

    pep8 de file

--- swmmio/version_control/utils.py
+++ swmmio/version_control/utils.py
@@ -6,7 +6,6 @@ import shutil
 from swmmio.utils import functions
 
 
-
 def copy_rpts_hsf(from_dir, to_dir, search_dir):
     """
     walk through a directory and find all rpts and hot start files and copy to
@@ -22,83 +21,75 @@ def copy_rpts_hsf(from_dir, to_dir, search_dir):
     Good for model results written on a local drive to a network drive
     """
 
-    #chain.from_iterable(os.walk(path) for path in paths):
+    # chain.from_iterable(os.walk(path) for path in paths):
     for path, dirs, files in os.walk(search_dir):
         for f in files:
             if '.rpt' in f:
-                rpt_path = os.path.join(path,f)
+                rpt_path = os.path.join(path, f)
                 to_dir = path.replace(from_dir, to_dir)
                 dest = os.path.join(to_dir, f)
                 shutil.copyfile(src=rpt_path, dst=dest)
 
             if '.hsf' in f:
-                hsf_path = os.path.join(path,f)
+                hsf_path = os.path.join(path, f)
                 to_dir = path.replace(from_dir, to_dir)
                 dest = os.path.join(to_dir, f)
                 shutil.copyfile(src=hsf_path, dst=dest)
 
 
-def write_inp_section(file_object, allheaders, sectionheader, section_data, pad_top=True, na_fill = ''):
-
+def write_inp_section(file_object, allheaders, sectionheader, section_data, pad_top=True, na_fill=''):
     """
-    given an open file object, excelwriter object, list of header sections, the curent
+    given an open file object, list of header sections, the current
     section header, and the section data in a Pandas Dataframe format, this function writes
     the data to the file object.
     """
 
     f = file_object
-    add_str =  ''
+    add_str = ''
     if not section_data.empty:
         if pad_top:
-            f.write('\n\n' + sectionheader + '\n') #add SWMM-friendly header e.g. [DWF]
+            f.write('\n\n' + sectionheader + '\n')  # add SWMM-friendly header e.g. [DWF]
         else:
             f.write(sectionheader + '\n')
         if allheaders and allheaders['headers'].get(sectionheader, 'blob') == 'blob':
-            #to left justify based on the longest string in the blob column
+            # to left justify based on the longest string in the blob column
             formatter = '{{:<{}s}}'.format(section_data[sectionheader].str.len().max()).format
             add_str = section_data.fillna('').to_string(
-                                                        index_names=False,
-                                                        header=False,
-                                                        index=False,
-                                                        justify='left',
-                                                        formatters={sectionheader:formatter}
-                                                        )
-            # #write section to excel sheet
-            # sheetname = sectionheader.replace('[', "").replace(']', "")
-            # section_data.to_excel(excelwriter, sheetname, index=False)
+                index_names=False,
+                header=False,
+                index=False,
+                justify='left',
+                formatters={sectionheader: formatter}
+            )
 
         else:
-            #naming the columns to the index name so the it prints in-line with col headers
+            # naming the columns to the index name so the it prints in-line with col headers
             f.write(';;')
-            #to left justify on longest string in the Comment column
-            #this is overly annoying, to deal with 'Objects' vs numbers to remove
-            #two bytes added from the double semicolon header thing (to keep things lined up)
-            objectformatter =   {hedr:' {{:<{}}}'.format(section_data[hedr].apply(str).str.len().max()).format
-                                    for hedr in section_data.columns}
-            numformatter =      {hedr:'  {{:<{}}}'.format(section_data[hedr].apply(str).str.len().max()).format
-                                    for hedr in section_data.columns if section_data[hedr].dtype!="O"}
+            # to left justify on longest string in the Comment column
+            # this is overly annoying, to deal with 'Objects' vs numbers to remove
+            # two bytes added from the double semicolon header thing (to keep things lined up)
+            objectformatter = {hedr: ' {{:<{}}}'.format(section_data[hedr].apply(str).str.len().max()).format
+                               for hedr in section_data.columns}
+            numformatter = {hedr: '  {{:<{}}}'.format(section_data[hedr].apply(str).str.len().max()).format
+                            for hedr in section_data.columns if section_data[hedr].dtype != "O"}
             objectformatter.update(numformatter)
             add_str = section_data.fillna(na_fill).to_string(
-                                                        index_names=False,
-                                                        header=True,
-                                                        justify='left',
-                                                        formatters=objectformatter#{'Comment':formatter}
-                                                        )
+                index_names=False,
+                header=True,
+                justify='left',
+                formatters=objectformatter  # {'Comment':formatter}
+            )
 
-            # #write section to excel sheet
-            # sheetname = sectionheader.replace('[', "").replace(']', "")
-            # section_data.to_excel(excelwriter, sheetname)
-
-
-        #write the dataframe as a string
+        # write the dataframe as a string
         f.write(add_str + '\n\n')
-def write_meta_data(file_object, metadicts):
 
+
+def write_meta_data(file_object, metadicts):
     file_object.write(json.dumps(metadicts, indent=4))
-    file_object.write('\n' + '='*100 + '\n\n')
+    file_object.write('\n' + '=' * 100 + '\n\n')
+
 
 def read_meta_data(filepath):
-
     s = ''
     with open(filepath) as file_object:
         for line in file_object:
@@ -107,6 +98,7 @@ def read_meta_data(filepath):
             s += line.strip()
 
     return json.loads(s)
+
 
 def bi_is_current(build_instr_file):
     """
@@ -117,7 +109,7 @@ def bi_is_current(build_instr_file):
     meta = read_meta_data(build_instr_file)
     baseline = meta['Parent Models']['Baseline']
     alternatives = meta['Parent Models']['Alternatives']
-    #parents = baseline.update(alternatives)
+    # parents = baseline.update(alternatives)
     # print meta['Parent Models']['Baseline']
     # print alternatives
     for inp, revisiondate in baseline.items():
@@ -130,6 +122,7 @@ def bi_is_current(build_instr_file):
 
     return True
 
+
 def bi_latest_parent_date_modified(vc_dir, parentname):
     """
     given a path to a version control directory of build instructions and the
@@ -138,7 +131,6 @@ def bi_latest_parent_date_modified(vc_dir, parentname):
     newest_bi = newest_file(vc_dir)
     meta = read_meta_data(newest_bi)
     # with open (newest_bi) as f:
-
 
     return meta['Parent Models'][parentname]
 
@@ -150,6 +142,7 @@ def newest_file(directory):
     """
     files = os.listdir(directory)
     return max([os.path.join(directory, f) for f in files], key=os.path.getctime)
+
 
 def modification_date(filename, string=True):
     """
@@ -163,18 +156,18 @@ def modification_date(filename, string=True):
     if string:
         return dt.strftime("%y-%m-%d %H:%M")
     else:
-        return dt#datetime.fromtimestamp(t)
+        return dt  # datetime.fromtimestamp(t)
+
 
 def write_excel_inp_section(excelwriter, allheaders, sectionheader, section_data):
-
     if not section_data.empty:
         sheetname = sectionheader.replace('[', "").replace(']', "")
 
         if allheaders['headers'].get(sectionheader, 'blob') == 'blob' and not section_data.empty:
-            #write section to excel sheet
+            # write section to excel sheet
             section_data.to_excel(excelwriter, sheetname, index=False)
         else:
-            #clean up the format a bit
+            # clean up the format a bit
             # workbook = excelwriter.book
             # elem_fmt = workbook.add_format({'align': 'left'})
             # worksheet = excelwriter.sheets[sheetname]
@@ -182,26 +175,25 @@ def write_excel_inp_section(excelwriter, allheaders, sectionheader, section_data
 
             section_data.to_excel(excelwriter, sheetname)
 
-def create_change_info_sheet(excelwriter, model1, model2, name='', message=''):
 
-    ix =  ['Date', 'BaselineModel', 'TargetModel', 'Commit', 'Name', 'Comment']
-    vals =  [
-            datetime.now().strftime("%y-%m-%d %H:%M"),
-            model1.inp.path,
-            model2.inp.path,
-            functions.random_alphanumeric(12),
-            name,
-            message
-            ]
-    df = pd.DataFrame(data=vals, index=ix,columns=['FileInfo'])
+def create_change_info_sheet(excelwriter, model1, model2, name='', message=''):
+    ix = ['Date', 'BaselineModel', 'TargetModel', 'Commit', 'Name', 'Comment']
+    vals = [
+        datetime.now().strftime("%y-%m-%d %H:%M"),
+        model1.inp.path,
+        model2.inp.path,
+        functions.random_alphanumeric(12),
+        name,
+        message
+    ]
+    df = pd.DataFrame(data=vals, index=ix, columns=['FileInfo'])
     df.to_excel(excelwriter, 'FileInfo')
 
 
 def create_info_sheet(excelwriter, basemodel, parent_models=[]):
-
-    #create an info sheet for the Excel file
+    # create an info sheet for the Excel file
     timeofcreation = datetime.now()
-    s = pd.Series([datetime.now(), basemodel.inp.path] + [x.inp.path for x in parent_models] )
-    s.index = ['DateCreated', 'Basemodel']+['ParentModel_' + str(i) for i,x in enumerate(parent_models)]
+    s = pd.Series([datetime.now(), basemodel.inp.path] + [x.inp.path for x in parent_models])
+    s.index = ['DateCreated', 'Basemodel'] + ['ParentModel_' + str(i) for i, x in enumerate(parent_models)]
     df = pd.DataFrame(s, columns=['FileInfo'])
     df.to_excel(excelwriter, 'FileInfo')
