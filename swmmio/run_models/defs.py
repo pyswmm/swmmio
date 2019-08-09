c1e13b3f7a8579c543acc7e5051642a8f8f68666
algchyhao <35864573+algchyhao@users.noreply.github.com>
2019-08-10 07:11:09 +0800

    future warning read_table -> read_csv
    read_table is deprecated

--- swmmio/run_models/defs.py
+++ swmmio/run_models/defs.py
@@ -7,26 +7,26 @@ current_dir = os.path.dirname(__file__)
 sets = j(current_dir, 'inp_settings')
 
 
-OPTIONS_no_rain = pd.read_table(
+OPTIONS_no_rain = pd.read_csv(
                                 j(sets, 'OPTIONS_no_rain.txt'),
                                 delim_whitespace=True,
                                 names=inp_header_dict['[OPTIONS]'].split(),
                                 skiprows=1,
                                 )
 
-OPTIONS_normal = pd.read_table(
+OPTIONS_normal = pd.read_csv(
                                 j(sets, 'OPTIONS_normal.txt'),
                                 delim_whitespace=True,
                                 names=inp_header_dict['[OPTIONS]'].split(),
                                 skiprows=1,
                                 )
-REPORT_nodes_links = pd.read_table(
+REPORT_nodes_links = pd.read_csv(
                                 j(sets, 'REPORT_nodes_links.txt'),
                                 delim_whitespace=True,
                                 names=inp_header_dict['[REPORT]'].split(),
                                 skiprows=1,
                                 )
-REPORT_none = pd.read_table(
+REPORT_none = pd.read_csv(
                                 j(sets, 'REPORT_none.txt'),
                                 delim_whitespace=True,
                                 names=inp_header_dict['[REPORT]'].split(),
