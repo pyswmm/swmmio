725330bb81665f738f19d679fffdaeefabdddd76
Bryant E. McDonnell <bemcdonnell@gmail.com>
2022-09-28 12:22:21 +0200

    First steps to Migrate to PySWMM as dependency

--- swmmio/run_models/run.py
+++ swmmio/run_models/run.py
@@ -4,20 +4,27 @@ import pandas as pd
 from swmmio.utils.modify_model import replace_inp_section
 from swmmio.run_models import defs
 from swmmio import Model
-from swmmio.defs.config import SWMM_ENGINE_PATH
+from swmmio.defs.config import PYTHON_EXE_PATH, PYSWMM_WRAPPER_PATH
 
 
-def run_simple(inp_path, swmm_eng=SWMM_ENGINE_PATH):
+def run_simple(inp_path, py_path=PYTHON_EXE_PATH, pyswmm_wrapper=PYSWMM_WRAPPER_PATH):
     """
     run a model once as is.
     """
-    print('running {} with {}'.format(inp_path, swmm_eng))
+    print('running {}'.format(inp_path))
     #inp_path = model.inp.path
     rpt_path = os.path.splitext(inp_path)[0] + '.rpt'
+    out_path = os.path.splitext(inp_path)[0] + '.out'
 
-    subprocess.call([swmm_eng, inp_path, rpt_path])
+    # Pass Environment Info to Run
+    env_definition = os.environ.copy()
+    env_definition["PATH"] = "/usr/sbin:/sbin:" + env_definition["PATH"]
 
-def run_hot_start_sequence(inp_path, swmm_eng=SWMM_ENGINE_PATH):
+    subprocess.call([py_path, pyswmm_wrapper, inp_path, rpt_path, out_path],
+                    env=env_definition)
+    return 0
+
+def run_hot_start_sequence(inp_path, py_path=PYTHON_EXE_PATH):
 
     # inp_path = model.inp.path
     model = Model(inp_path)
@@ -33,7 +40,7 @@ def run_hot_start_sequence(inp_path, swmm_eng=SWMM_ENGINE_PATH):
     model = replace_inp_section(model.inp.path, '[FILES]', hot1_df)
     model = replace_inp_section(model.inp.path, '[REPORT]', defs.REPORT_none)
     model = replace_inp_section(model.inp.path, '[OPTIONS]', defs.OPTIONS_no_rain)
-    subprocess.call([swmm_eng, model.inp.path, rpt_path])
+    run_simple(model.inp.path)
 
     # if os.path.exists(hotstart1) and not os.path.exists(hotstart2):
     #create new model inp with params to use hotstart1 and save hotstart2
@@ -41,7 +48,7 @@ def run_hot_start_sequence(inp_path, swmm_eng=SWMM_ENGINE_PATH):
     s = pd.Series(['USE HOTSTART "{}"'.format(hotstart1), 'SAVE HOTSTART "{}"'.format(hotstart2)])
     hot2_df = pd.DataFrame(s, columns=['[FILES]'])
     model = replace_inp_section(model.inp.path, '[FILES]', hot2_df)
-    subprocess.call([swmm_eng, model.inp.path, rpt_path])
+    run_simple(model.inp.path)
 
     # if os.path.exists(hotstart2):
     #create new model inp with params to use hotstart2 and not save anything
@@ -53,4 +60,4 @@ def run_hot_start_sequence(inp_path, swmm_eng=SWMM_ENGINE_PATH):
     model = replace_inp_section(model.inp.path, '[REPORT]', defs.REPORT_none)# defs.REPORT_nodes_links)
     model = replace_inp_section(model.inp.path, '[OPTIONS]', defs.OPTIONS_normal)
 
-    subprocess.call([swmm_eng, model.inp.path, rpt_path])
+    run_simple(model.inp.path)
