c3716f8f63f596370bd608df8a3bd954fd1b5c41
Adam Erispaha <aerispaha@gmail.com>
2024-12-02 20:27:46 -0500

    Restore dev version

--- swmmio/__init__.py
+++ swmmio/__init__.py
@@ -11,7 +11,7 @@ from swmmio.graphics.profiler import (build_profile_plot, add_hgl_plot,
 '''Python SWMM Input/Output Tools'''
 
 
-VERSION_INFO = (0, 7, 3)
+VERSION_INFO = (0, 7, 4, 'dev0')
 __version__ = '.'.join(map(str, VERSION_INFO))
 __author__ = 'Adam Erispaha'
 __copyright__ = 'Copyright (c) 2024'
